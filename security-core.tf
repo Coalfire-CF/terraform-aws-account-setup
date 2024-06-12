7805815ad49d6ea7a9473e5d92596e3164f17561
Herman Wong <herman.wong@coalfire.com>
2024-06-12 13:39:51 -0700

    Update SecurityCore

--- security-core.tf
+++ security-core.tf
@@ -1,8 +1,11 @@
 module "security-core" {
-  source = "github.com/Coalfire-CF/terraform-aws-securitycore?ref=v0.0.17"
+  source = "github.com/Coalfire-CF/terraform-aws-securitycore?ref=02087ae72394cd06431efc5dbbc4bf1f7f88ad14"
 
-  account_number              = var.account_number
   application_account_numbers = var.application_account_numbers
   aws_region                  = var.aws_region
   resource_prefix             = var.resource_prefix
+
+  # KMS Keys
+  dynamo_kms_key_arn = module.dynamo_kms_key[0].kms_key_arn
+  s3_kms_key_arn     = module.s3_kms_key[0].kms_key_arn
 }

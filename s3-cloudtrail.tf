e759c68e35580001bf85da0ab15b9691d80277b9
Merle Infanger <merle.infanger@coalfire.com>
2026-02-24 10:04:11 -0700

    Module ref update

--- s3-cloudtrail.tf
+++ s3-cloudtrail.tf
@@ -1,5 +1,5 @@
 module "s3-cloudtrail" {
-  source = "git::https://github.com/Coalfire-CF/terraform-aws-s3?ref=resource-naming-cleanup"
+  source = "git::https://github.com/Coalfire-CF/terraform-aws-s3?ref=v1.1.1"
 
   count = var.create_cloudtrail && var.default_aws_region == var.aws_region ? 1 : 0
 

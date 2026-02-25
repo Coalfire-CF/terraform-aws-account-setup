dd31d4d720d1158db0c01bebce244f41af6144bf
Merle Infanger <merle.infanger@coalfire.com>
2026-02-25 10:37:20 -0700

    fix: provider version resolution

--- s3-accesslog.tf
+++ s3-accesslog.tf
@@ -3,7 +3,7 @@ module "s3-accesslogs" {
   count = var.create_s3_accesslogs_bucket ? 1 : 0
 
   #checkov:skip=CKV_AWS_145: "Ensure that S3 buckets are encrypted with KMS by default"
-  source = "git::https://github.com/Coalfire-CF/terraform-aws-s3?ref=provider-v6-imp"
+  source = "git::https://github.com/Coalfire-CF/terraform-aws-s3?ref=v1.1.2"
 
   name                    = local.accesslogs_bucket_name
   attach_public_policy    = false
